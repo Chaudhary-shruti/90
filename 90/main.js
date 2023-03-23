dcdbdb908653a379707ba783a339fcdce451b66b
Chaudhary-shruti <93135302+Chaudhary-shruti@users.noreply.github.com>
2023-03-23 09:24:34 -0700

    Add files via upload

---
+++ 90/main.js
@@ -0,0 +1,11 @@
+player1=""
+player2=""
+
+
+ function addUser(){
+document.getElementById("input").value
+document.getElementById("input2").value
+localStorage.setItem("player1", player1)
+localStorage.setItem("player2", player2)
+windows.location="game.html"
+}
